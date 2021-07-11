be06e829cb58bc3df6e1bd5d092edd24ee937899
cddu90 <66915894+cddu90@users.noreply.github.com>
2021-07-10 20:21:00 -0700

    Update HibernateUtil.java

--- src/main/test/com/qa/util/HibernateUtil.java
+++ src/main/test/com/qa/util/HibernateUtil.java
@@ -1,4 +1,25 @@
 
-public class HibernateUtil {
+package com.bsc.qa.facets.bor_file_generator_stt.util;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+import com.bsc.qa.facets.afa.pojo.Connection;
+
+
+public class HibernateUtil {
+	
+	
+	public static SessionFactory createSessionFactory(Connection conn){
+		Configuration configuration = new Configuration().configure();
+		configuration.getProperties().setProperty("hibernate.connection.username", conn.getUsername());
+		configuration.getProperties().setProperty("hibernate.connection.password", conn.getPassword());
+		configuration.getProperties().setProperty("hibernate.connection.url", conn.getUrl());
+		ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+		SessionFactory factory = configuration.buildSessionFactory(serviceRegistry);
+		return factory;
+	}
+		
 }
