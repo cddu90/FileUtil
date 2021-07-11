26a6c66f3a6547738a5e8e0dc1fb319e9444d1da
cddu90 <66915894+cddu90@users.noreply.github.com>
2021-07-10 20:20:37 -0700

    Update HelperClass.java

--- src/main/test/com/qa/util/HelperClass.java
+++ src/main/test/com/qa/util/HelperClass.java
@@ -1,4 +1,284 @@
 
+package com.bsc.qa.facets.bor_file_generator_stt.util;
+
+import java.io.File;
+import java.math.BigDecimal;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Random;
+import java.util.TimeZone;
+import java.util.concurrent.ThreadLocalRandom;
+
+import com.bsc.qa.facets.afa.pojo.AmountFields;
+import com.bsc.qa.facets.afa.pojo.BorAdjustmentDB;
+
+
+
 public class HelperClass {
 
+	private static String dd;
+	private static String mm;
+	private static String hh;
+	private static String mn;
+	private static String sc;
+
+	
+	
+	
+	public static String[] getBorFileName( ){
+		Date d = new Date();
+		Calendar cal = Calendar.getInstance(Locale.US);
+		cal.setTime(d);
+		TimeZone tz = TimeZone.getTimeZone("America/Los_Angeles");
+		cal.setTimeZone(tz);
+
+		int dd_i = cal.get(Calendar.DATE);
+		if (dd_i < 10) {
+			dd = "0" + dd_i;
+		} else {
+			dd = "" + dd_i;
+		}
+
+		int mm_i = cal.get(Calendar.MONTH);
+		mm_i += 1;
+		if (mm_i < 10) {
+			mm = "0" + mm_i;
+		} else {
+			mm = "" + mm_i;
+		}
+
+		int clm_id = 0;
+		Random r = new Random();
+		int x = r.nextInt(999999);
+		if (x < 100000) {
+			x = x + 100000;
+		}
+		clm_id = x;
+		int yy = cal.get(Calendar.YEAR);
+		int hh_i = cal.get(Calendar.HOUR);
+		if (hh_i < 10) {
+			hh = "0" + hh_i;
+		} else {
+			hh = "" + hh_i;
+		}
+		
+		int mn_i = cal.get(Calendar.MINUTE);
+		if(mn_i < 10){
+			mn = "0" + mn_i;
+		}else{
+			mn = "" + mn_i;
+		}
+		
+		int sc_i = cal.get(Calendar.SECOND);
+		if(sc_i < 10){
+			sc = "0" + sc_i;
+		}else{
+			sc = "" + sc_i;
+		}
+		
+		yy = yy % 100;
+		File dir=new File("Output files");
+		if(!dir.exists())
+		dir.mkdir();
+		//String borFileName = "FACETS_PCT_AFAGL." + "#" + mm + dd + yy + "." + hh + mn + sc + ".txt";
+		String[] FileName = {"FCT_VNDCLM_CVS_"+ yy  + mm + dd+ "." + hh + mn + sc + ".txt","FACETS_PCT_AFAGL." + "#" + mm + dd + yy + "." + hh + mn + sc + ".txt"};
+		
+		return FileName;
+	}
+	
+	public static String getTodaysDateFake(Date dbCheckDate ){
+		long DAY_IN_MS = 1000 * 60 * 60 * 24;
+		Date checkDate = new Date(dbCheckDate.getTime() + (2 * DAY_IN_MS));
+//		Date d = new Date();
+//		Calendar cal = Calendar.getInstance(Locale.US);
+//		cal.setTime(d);
+//		TimeZone tz = TimeZone.getTimeZone("America/Los_Angeles");
+//		cal.setTimeZone(tz);
+//		
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MMM-yy");
+		String send = dateFormat.format(checkDate);
+//		System.out.println(dateFormat.format(d).toUpperCase());
+		return send;
+	}
+	public static String getTodaysDate(){
+		Date d = new Date();
+		Calendar cal = Calendar.getInstance(Locale.US);
+		cal.setTime(d);
+		TimeZone tz = TimeZone.getTimeZone("America/Los_Angeles");
+		cal.setTimeZone(tz);
+		String returnString = "";
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MMM-yy");
+		try {
+			Date sysDate = dateFormat.parse(dateFormat.format(d).toUpperCase());
+			Date send = dateFormat.parse("25-JUL-19");
+			if(sysDate.before(send)==true){
+				returnString = "25-JUL-19";
+			}else{
+				returnString = dateFormat.format(sysDate).toUpperCase();
+			}
+		} catch (ParseException e) {
+			System.out.println("Unable to parse sysDate");
+			e.printStackTrace();
+		}
+//		System.out.println(dateFormat.format(d).toUpperCase());
+		return returnString;
+	}
+	public static String getLastYearsDate(){
+		Date d = new Date();
+		Calendar cal = Calendar.getInstance(Locale.US);
+		cal.setTime(d);
+	
+		TimeZone tz = TimeZone.getTimeZone("America/Los_Angeles");
+		cal.setTimeZone(tz);
+		
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MMM-");
+		SimpleDateFormat dateFormat1 = new SimpleDateFormat("yy");
+		String yearString = String.valueOf((Integer.parseInt(dateFormat1.format(d))-1));
+//		System.out.println(dateFormat.format(d).toUpperCase());
+		return dateFormat.format(d).toUpperCase().concat(yearString);
+	}
+	
+	public static AmountFields getAmountFields(BigDecimal billedAmount){
+		AmountFields amountFields = new AmountFields();
+		//ingredientCost
+		//BigDecimal billedAmount =(new BigDecimal(ThreadLocalRandom.current().nextInt(400, 999 + 1))).setScale(2, BigDecimal.ROUND_HALF_UP);
+		BigDecimal ingredientCost = billedAmount;//.divide(new BigDecimal(2)).setScale(2, BigDecimal.ROUND_HALF_UP);
+		BigDecimal allowedAmount = billedAmount.multiply((new BigDecimal(0.6))).setScale(2, BigDecimal.ROUND_HALF_UP);
+		BigDecimal deductible = billedAmount.multiply((new BigDecimal(0.2))).setScale(2, BigDecimal.ROUND_HALF_UP);
+		BigDecimal copay = billedAmount.multiply((new BigDecimal(0.1))).setScale(2, BigDecimal.ROUND_HALF_UP);
+		BigDecimal coinsurance = billedAmount.multiply((new BigDecimal(0.1))).setScale(2, BigDecimal.ROUND_HALF_UP);
+		BigDecimal claimAmount = ingredientCost.subtract(deductible).subtract(copay).setScale(2, BigDecimal.ROUND_HALF_UP);
+		BigDecimal clientPrice = billedAmount;
+		BigDecimal bscRevenueAmount = clientPrice.subtract(claimAmount).setScale(2, BigDecimal.ROUND_HALF_UP);
+		
+		
+		amountFields.setClaimAmount(String.valueOf(claimAmount));
+		amountFields.setBilledAmount(String.valueOf(billedAmount));
+		amountFields.setDeductible(String.valueOf(deductible));
+		amountFields.setCopay(String.valueOf(copay));
+		amountFields.setCoInsurance(String.valueOf(coinsurance));
+		amountFields.setClientPrice(String.valueOf(clientPrice));
+		amountFields.setBscRevenueAmount(String.valueOf(bscRevenueAmount));
+		amountFields.setAllowedAmount(String.valueOf(allowedAmount));
+		
+//		System.out.println(amountFields);
+		return amountFields;
+	}
+
+	public static AmountFields getAdjustmentAmountFields(BigDecimal billedAmount){
+		AmountFields amountFields = new AmountFields();
+		
+		//BigDecimal billedAmount =(new BigDecimal(ThreadLocalRandom.current().nextInt(400, 999 + 1))).setScale(2, BigDecimal.ROUND_HALF_UP);
+		//BigDecimal ingredientCost = billedAmount.divide(new BigDecimal(2)).setScale(2, BigDecimal.ROUND_HALF_UP);
+		BigDecimal allowedAmount = billedAmount.multiply((new BigDecimal(0.6))).setScale(2, BigDecimal.ROUND_HALF_UP);
+		BigDecimal deductible = billedAmount.multiply((new BigDecimal(0.2))).setScale(2, BigDecimal.ROUND_HALF_UP);
+		BigDecimal copay = billedAmount.multiply((new BigDecimal(0.1))).setScale(2, BigDecimal.ROUND_HALF_UP);
+		BigDecimal coinsurance = billedAmount.multiply((new BigDecimal(0.1))).setScale(2, BigDecimal.ROUND_HALF_UP);
+		BigDecimal claimAmount = billedAmount.divide(new BigDecimal(2)).setScale(2 , BigDecimal.ROUND_HALF_UP);
+		BigDecimal clientPrice = billedAmount;
+		BigDecimal bscRevenueAmount = claimAmount;
+		
+		amountFields.setClaimAmount(String.valueOf(claimAmount));
+		amountFields.setBilledAmount(String.valueOf(billedAmount));
+		amountFields.setDeductible(String.valueOf(deductible));
+		amountFields.setCopay(String.valueOf(copay));
+		amountFields.setCoInsurance(String.valueOf(coinsurance));
+		amountFields.setClientPrice(String.valueOf(clientPrice));
+		amountFields.setBscRevenueAmount(String.valueOf(bscRevenueAmount));
+		amountFields.setAllowedAmount(String.valueOf(allowedAmount));
+		
+//		System.out.println(amountFields);
+		return amountFields;
+	}
+	
+	public static AmountFields getAdjustmentAmountFieldsScenario3(BigDecimal billedAmount,BorAdjustmentDB adjustmentRecord){
+		AmountFields amountFields = new AmountFields();
+		
+		//BigDecimal billedAmount =(new BigDecimal(ThreadLocalRandom.current().nextInt(400, 999 + 1))).setScale(2, BigDecimal.ROUND_HALF_UP);
+		//BigDecimal ingredientCost = billedAmount.divide(new BigDecimal(2)).setScale(2, BigDecimal.ROUND_HALF_UP);
+		BigDecimal allowedAmount = billedAmount.multiply((new BigDecimal(0.6))).setScale(2, BigDecimal.ROUND_HALF_UP);
+		BigDecimal deductible = billedAmount.multiply((new BigDecimal(0.2))).setScale(2, BigDecimal.ROUND_HALF_UP);
+		BigDecimal copay = billedAmount.multiply((new BigDecimal(0.1))).setScale(2, BigDecimal.ROUND_HALF_UP);
+		BigDecimal coinsurance = billedAmount.multiply((new BigDecimal(0.1))).setScale(2, BigDecimal.ROUND_HALF_UP);
+		BigDecimal claimAmount = billedAmount.divide(new BigDecimal(2)).setScale(2 , BigDecimal.ROUND_HALF_UP);
+		BigDecimal clientPrice = billedAmount;
+		BigDecimal bscRevenueAmount = claimAmount;
+		
+		if(adjustmentRecord.getCLM_AMT().compareTo(claimAmount)==1){
+//			System.out.println(adjustmentRecord.getCLM_AMT()+" adjustmentRecord.getCLM_AMT()");
+		amountFields.setClaimAmount(String.valueOf(claimAmount));
+		}else{
+			amountFields.setClaimAmount(String.valueOf(adjustmentRecord.getCLM_AMT()));
+		}
+		if(adjustmentRecord.getBIL_AMT().compareTo(billedAmount)==1){
+//			System.out.println(adjustmentRecord.getBIL_AMT()+" adjustmentRecord.getBIL_AMT()");
+		amountFields.setBilledAmount(String.valueOf(billedAmount));
+		}else{
+			amountFields.setBilledAmount(String.valueOf(adjustmentRecord.getBIL_AMT()));
+		}
+		if(adjustmentRecord.getDED_AMT().compareTo(deductible)==1){
+//			System.out.println(adjustmentRecord.getDED_AMT()+" adjustmentRecord.getDED_AMT()");
+		amountFields.setDeductible(String.valueOf(deductible));
+		}else{
+			amountFields.setDeductible(String.valueOf(adjustmentRecord.getDED_AMT()));
+		}
+		if(adjustmentRecord.getCOPAY_AMT().compareTo(copay)==1){
+//			System.out.println(adjustmentRecord.getCOPAY_AMT()+" adjustmentRecord.getCOPAY_AMT()");
+		amountFields.setCopay(String.valueOf(copay));
+		}else{
+			amountFields.setCopay(String.valueOf(adjustmentRecord.getCOPAY_AMT()));
+		}
+		if(adjustmentRecord.getCOINS_AMT().compareTo(coinsurance)==1){
+//			System.out.println(adjustmentRecord.getCOINS_AMT()+" adjustmentRecord.getCOINS_AMT()");
+		amountFields.setCoInsurance(String.valueOf(coinsurance));
+		}else{
+			amountFields.setCoInsurance(String.valueOf(adjustmentRecord.getCOINS_AMT()));
+		}
+		if(adjustmentRecord.getCLI_PRC_AMT().compareTo(clientPrice)==1){
+//			System.out.println(adjustmentRecord.getCLI_PRC_AMT()+" adjustmentRecord.getCLI_PRC_AMT()");
+		amountFields.setClientPrice(String.valueOf(clientPrice));
+		}else{
+			amountFields.setClientPrice(String.valueOf(adjustmentRecord.getCLI_PRC_AMT()));
+		}
+		if(adjustmentRecord.getBSC_RVNU_AMT().compareTo(bscRevenueAmount)==1){
+//			System.out.println(adjustmentRecord.getBSC_RVNU_AMT()+" adjustmentRecord.getBSC_RVNU_AMT()");
+			amountFields.setBscRevenueAmount(String.valueOf(bscRevenueAmount));
+		}else{
+			amountFields.setBscRevenueAmount(String.valueOf(adjustmentRecord.getBSC_RVNU_AMT()));
+		}
+		if(adjustmentRecord.getALLOW_AMT().compareTo(allowedAmount)==1){
+//			System.out.println(adjustmentRecord.getALLOW_AMT()+" adjustmentRecord.getALLOW_AMT()");
+		amountFields.setAllowedAmount(String.valueOf(allowedAmount));
+		}else{
+			amountFields.setAllowedAmount(String.valueOf(adjustmentRecord.getALLOW_AMT()));
+		}
+		
+//		System.out.println(amountFields);
+		return amountFields;
+	}
+
+	public static int getDecimalForAmountFields(){
+		int randomDecimals = ThreadLocalRandom.current().nextInt(10, 99 + 1);
+			return randomDecimals;
+		
+		}
+
+	public static String getTodaysPrior1MDate() {
+		// TODO Auto-generated method stub
+		Date d = new Date();
+		String todayPrior3MDate ="";
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-MMM-yy");
+		Calendar cal =  Calendar.getInstance(Locale.US);
+		//System.out.println(sdf.format(cal.getTime()));
+		TimeZone tz = TimeZone.getTimeZone("America/Los_Angeles");
+		cal.setTimeZone(tz);
+        cal.add(Calendar.MONTH, -1);
+         todayPrior3MDate= sdf.format(cal.getTime()).toUpperCase();
+		return todayPrior3MDate;
+	}
+	
 }
+
