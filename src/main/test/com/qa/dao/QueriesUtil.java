ac9751107cd9d90b773ac4f51404aaf1c5c76232
cddu90 <66915894+cddu90@users.noreply.github.com>
2021-07-10 20:15:07 -0700

    Update QueriesUtil.java

--- src/main/test/com/qa/dao/QueriesUtil.java
+++ src/main/test/com/qa/dao/QueriesUtil.java
@@ -1,4 +1,45 @@
 
-public class QueriesUtil {
+package com.bsc.qa.facets.afa.dao;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class QueriesUtil {
+	
+	public Map<String, String> queriesMap(){
+		File file = new File(new File("").getAbsolutePath()+"\\src\\test\\resources\\bor-queries-app.csv");
+		Map<String, String> map=new HashMap<String, String>();
+		List<List<String>> records = new ArrayList<>();
+		try (Scanner scanner = new Scanner(file);) {
+			while (scanner.hasNextLine()) {
+				records.add(getRecordFromLine(scanner.nextLine()));
+			}
+			for (List<String> list : records) {
+				map.put(list.get(0), list.get(1));
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return map;
+	}
+	public List<String> getRecordFromLine(String line) {
+		List<String> values = new ArrayList<String>();
+		
+		try (Scanner rowScanner = new Scanner(line)) {
+			rowScanner.useDelimiter(":");
+			while (rowScanner.hasNext()) {
+				values.add(rowScanner.next());	
+			}
+			
+		}
+		return values;
+	}
+	
 }
+
